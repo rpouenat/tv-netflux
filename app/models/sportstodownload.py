734af0370771c9d2a5d60f7abc37daff41a648cc
super-dev-part2 <justice-poro@hotmail.fr>
2024-03-07 19:57:00 +0100

    Update

--- app/models/sportstodownload.py
+++
@@ -1,6 +0,0 @@
-from .. import db
-
-class SportsToDownload(db.Model):
-
-    idYgg = db.Column(db.Integer, primary_key=True) # id du film
-    downloaded = db.Column(db.Integer, nullable=False) # Permet de savoir si le sport a été téléchargé
--- app/models/tvshowtodownload.py
+++
@@ -1,9 +0,0 @@
-from .. import db
-
-class TvShowToDownload(db.Model):
-
-    idTvShow = db.Column(db.Integer, primary_key=True) # id de la série
-    id_imdb = db.Column(db.Integer, nullable=False) # ID imdb de la série
-    current_season = db.Column(db.Integer, nullable=True) # Saison en cours
-    current_episode = db.Column(db.Integer, nullable=True) # Episode en cours
-    downloaded = db.Column(db.Integer, nullable=True) # Permet de savoir si l'épisode a été téléchargé ou non
