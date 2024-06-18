281db0e9e9cd52fe8f2fe13ded558ba6e4901f24
super-dev-part2 <justice-poro@hotmail.fr>
2024-06-18 10:02:21 +0200

    Update

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,7 +4,8 @@ api = Blueprint('api', __name__)
 
 # Import any endpoints here to make them available
 from . import main
-from .eurosport import eurosport
+# from .eurosport import eurosport
+from .eurosport import eurosport_max
 from .test_drm import sample
 from .groupem6 import m6
 from .groupecanal import canal
